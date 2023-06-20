fd9d4085f92520df75f69c9db9a036f26a34ae21
amoguelk <amoguelk@hotmail.com>
2023-06-19 18:37:32 -0600

    Comparación de llave

--- ClienteCifrado01.java
+++ ClienteCifrado01.java
@@ -2,86 +2,118 @@ import java.net.*;
 import java.io.*;
 import java.security.*;
 import javax.crypto.*;
+import java.util.Arrays;
 
 public class ClienteCifrado01
 {
+  static int KEY_CORRECT = 0;
+  static int KEY_INCORRECT = 1;
   public static void main(String a[]) throws Exception
   {
+
+    if (a.length != 1) {
+      printError("USO: java ClienteCifrado01 <host>");
+    }
+
     Socket socket = null;
-    // Peticion es lo que envia el Cliente
     String peticion = "";
+    byte respuesta[] = new byte[10000];
 
-    // Peticion es lo que envia el Servidor
-    byte arreglo[] = new byte[10000];
+    ObjectInput in = null;
 
-    try
-    {
-      
-      ObjectInput in = new ObjectInputStream(new FileInputStream("llave.ser"));
-      Key llave = (Key)in.readObject();
-      System.out.println( "llave=" + llave );
-      in.close();
-      
-      System.out.println("Me conecto al puerto 8000 del servidor");
-      socket = new Socket(a[0],8000);
-      // Como ya hay socket, obtengo los flujos asociados a este
-      DataInputStream dis = new DataInputStream( socket.getInputStream() );
-      DataOutputStream dos = new DataOutputStream( socket.getOutputStream() );
+    try {
+      in = new ObjectInputStream(new FileInputStream("llave.ser"));
+    } catch (FileNotFoundException e) {
+      printError("Error al obtener el archivo de la llave.");
+    }
+    
+    Key llave = (Key)in.readObject();
+    System.out.println( "llave=" + llave );
+    in.close();
+    
+    System.out.println("Me conecto al puerto 8000 del servidor");
+    socket = new Socket(a[0],8000);
+
+    DataInputStream dis = null;
+    DataOutputStream dos = null;
+
+    try {
+      dis = new DataInputStream( socket.getInputStream() );
+      dos = new DataOutputStream( socket.getOutputStream() );
+    } catch (ConnectException e) {
+      printError("Error al conectarse al servidor. Compruebe que el servidor esté encendido.");
+    }
+    
+    /*
+      COMPROBACIÓN DE LLAVE
+    */
+    int keyHash = llave.hashCode();
+    System.out.println(keyHash);
+    dos.writeInt(keyHash);
+    if (dis.readInt() != KEY_CORRECT) {
+      printError("Error en la llave. Compruebe que tenga la llave más reciente generada por el servidor.");
+    }
+
+    try {
       // Ya que me conecte con el Servidor, 
       //debo leer del teclado para despues eso mismo enviarlo al Servidor
-      System.out.println("");
-      System.out.print( "Envía un mensaje al servidor > " );
-      BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
-      peticion = br.readLine();
-      
-      System.out.println( "===========================" );
-      System.out.println("");
-      System.out.println( "Se ha enviado el mensaje: " + peticion );
-      //System.out.println( "Cifraremos el mensaje..." );
-      byte[] arrayPeticion = peticion.getBytes();
-      Cipher cifrar = Cipher.getInstance("DES");
-      cifrar.init(Cipher.ENCRYPT_MODE, llave);
-      byte[] cipherText = cifrar.doFinal( arrayPeticion );
-      System.out.print( "El argumento CIFRADO es: " );
-      // NO SE DEBE PASAR A String
-      // System.out.println( new String( cipherText ) );
-      for(int i=0; i < cipherText.length; i++)
-        System.out.print( (char)cipherText[i] );
-      System.out.println( "" );
-      // Como yo escribo la peticion a la red,
-      // el Servidor debe leer de la red
-      System.out.println( " " );
-      System.out.println( "Lectura Byte a Byte: " );
-      bytesToBits( cipherText );
-      dos.write( cipherText, 0, cipherText.length );
-      System.out.println("");
-      System.out.println( "===========================" );
+      // Ciclo para hablar con el servidor 
+      while (true) {
+        System.out.println("");
+        System.out.print( "Envía un mensaje al servidor > " );
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+        peticion = br.readLine();
+        if (peticion.equals("exit")) break;
+        
+        System.out.println( "===========================" );
+        System.out.println("");
+        System.out.println( "Se ha enviado el mensaje: " + peticion );
+        //System.out.println( "Cifraremos el mensaje..." );
+        byte[] arrayPeticion = peticion.getBytes();
+        Cipher cifrar = Cipher.getInstance("DES");
+        cifrar.init(Cipher.ENCRYPT_MODE, llave);
+        byte[] cipherText = cifrar.doFinal( arrayPeticion );
+        System.out.print( "El argumento CIFRADO es: " );
+        // NO SE DEBE PASAR A String
+        // System.out.println( new String( cipherText ) );
+        for(int i=0; i < cipherText.length; i++)
+          System.out.print( (char)cipherText[i] );
+        System.out.println( "" );
+        // Como yo escribo la peticion a la red,
+        // el Servidor debe leer de la red
+        System.out.println( " " );
+        System.out.println( "Lectura Byte a Byte: " );
+        bytesToBits( cipherText );
+        dos.write( cipherText, 0, cipherText.length );
+        System.out.println("");
+        System.out.println( "===========================" );
 
 
-      //Ahora Cliente lee lo que servidor mande
-        int bytesLeidos = dis.read(arreglo);
+        //Ahora Cliente lee lo que servidor mande
+        int bytesLeidos = dis.read(respuesta);
         System.out.println("bytes leidos=" + bytesLeidos);
-        byte arreglo2[]  = new byte[bytesLeidos];
+        byte respuesta2[]  = new byte[bytesLeidos];
         for(int i=0; i < bytesLeidos; i++ )
         {
-          arreglo2[i] = arreglo[i];
+          respuesta2[i] = respuesta[i];
         }
 
         cifrar = Cipher.getInstance("DES");
         cifrar.init(Cipher.DECRYPT_MODE, llave);
-        bytesToBits( arreglo2 );
-        byte[] newPlainText = cifrar.doFinal(arreglo2);
+        bytesToBits( respuesta2 );
+        byte[] newPlainText = cifrar.doFinal(respuesta2);
         System.out.println( "El argumento DESENCRIPTADO es:" );
         // NO SE DEBE PASAR A String
         // System.out.println( new String(newPlainText, "UTF8") );
         for(int i=0; i < newPlainText.length; i++)  
           System.out.print( (char)newPlainText[i] );
         System.out.println(" ");
+      }
       
       //Cerrar conexión
-      //dos.close();
-      //dis.close();
-      //socket.close();
+      dos.close();
+      dis.close();
+      socket.close();
     }
     catch(IOException e)
     {
@@ -90,7 +122,19 @@ public class ClienteCifrado01
     }
   }
 
-  public static void bytesToBits( byte[] texto )
+  static byte[] cifrar(byte[] textoClaro, Key llave) throws Exception {
+    Cipher cifrar = Cipher.getInstance("DES");
+    cifrar.init(Cipher.ENCRYPT_MODE, llave);
+    return cifrar.doFinal(textoClaro);
+  }
+
+  static byte[] descifrar(byte[] textoCifrado, Key llave) throws Exception {
+    Cipher descifrar = Cipher.getInstance("DES");
+    descifrar.init(Cipher.DECRYPT_MODE, llave);
+    return descifrar.doFinal(textoCifrado);
+  }
+
+  static void bytesToBits( byte[] texto )
   {
     StringBuilder stringToBits = new StringBuilder();
     for( int i=0; i < texto.length; i++ )
@@ -110,4 +154,8 @@ public class ClienteCifrado01
     System.out.println( "El mensaje completo en bits es: " + stringToBits );
   }
 
+  static void printError(String msg) {
+    System.out.println(msg);
+    System.exit(1);
+  }
 }
--- ServidorCifrado01.java
+++ ServidorCifrado01.java
@@ -2,15 +2,18 @@ import java.net.*;
 import java.io.*;
 import java.security.*;
 import javax.crypto.*;
+import java.util.Arrays;
 
 public class ServidorCifrado01
 {
+  static int KEY_CORRECT = 0;
+  static int KEY_INCORRECT = 1;
   public static void main(String a[]) throws Exception
   {
     ServerSocket serverSocket = null;
     Socket socket = null;
     // Peticion es lo que envia el Cliente
-    byte arreglo[] = new byte[10000];
+    byte peticion[] = new byte[10000];
 
     System.out.println( "Generando la llave..." );
     KeyGenerator keyGen = KeyGenerator.getInstance("DES");
@@ -36,6 +39,7 @@ public class ServidorCifrado01
     }
 
     System.out.println("Esperando a que los clientes se conecten...");
+    // Ciclo para estar en línea
     while(true)
     {
       try
@@ -43,53 +47,75 @@ public class ServidorCifrado01
         socket = serverSocket.accept();
         System.out.println("\nSe conecto un cliente: " + socket.getInetAddress().getHostName());
         System.out.println("");
-        // Como ya hay socket, obtengo los flujos asociados a este
-        DataInputStream dis = new DataInputStream( socket.getInputStream() );
-        DataOutputStream dos = new DataOutputStream( socket.getOutputStream() );
-        // Despues de la conexion, Servidor y Cliente deben ponerse de acuerdo
-        // para ver quien escribe primero y entonces el otro debe leer
-        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
-        // Como el Cliente escribe, yo debo leer
-        int bytesLeidos = dis.read(arreglo);
-        System.out.println("bytes leidos: " + bytesLeidos);
-        byte arreglo2[]  = new byte[bytesLeidos];
-        for(int i=0; i < bytesLeidos; i++ )
-        {
-          arreglo2[i] = arreglo[i];
-        }
+        DataInputStream dis = null;
+        DataOutputStream dos = null;
+        // Ciclo para hablar con el cliente 
+        while (true) {
+          // Como ya hay socket, obtengo los flujos asociados a este
+          dis = new DataInputStream( socket.getInputStream());
+          dos = new DataOutputStream( socket.getOutputStream());
 
-        Cipher cifrar = Cipher.getInstance("DES");
-        cifrar.init(Cipher.DECRYPT_MODE, llave);
-        System.out.println("");
-        bytesToBits( arreglo2 );
-        byte[] newPlainText = cifrar.doFinal(arreglo2);
-        System.out.print( "El mensaje DESENCRIPTADO es: " );
-        // NO SE DEBE PASAR A String
-        // System.out.println( new String(newPlainText, "UTF8") );
-        for(int i=0; i < newPlainText.length; i++)
-          System.out.print( (char)newPlainText[i] );
-        
-        System.out.println("");
-        System.out.println( "\n===========================" );
-        System.out.print("> ");
-        String respuesta = br.readLine();
-        System.out.println( "Mi respuesta es: " + respuesta );
-        System.out.println( "Ahora encriptamos la respuesta..." );
-        byte[] arrayRespuesta = respuesta.getBytes();
-        cifrar = Cipher.getInstance("DES");
-        cifrar.init(Cipher.ENCRYPT_MODE, llave);
-        byte[] cipherText = cifrar.doFinal( arrayRespuesta );
-        System.out.println( "El argumento ENCRIPTADO es:" );
-        // NO SE DEBE PASAR A String
-        // System.out.println( new String( cipherText ) );
-        for(int i=0; i < cipherText.length; i++)
-          System.out.print( (char)cipherText[i] );
-        System.out.println( "" );
-        // Como yo escribo la peticion a la red,
-        // el Servidor debe leer de la red
-        bytesToBits( cipherText );
-        dos.write( cipherText, 0, cipherText.length );
-        System.out.println("\n");
+          /*
+            COMPROBACIÓN DE LLAVE
+          */
+          int keyHash = llave.hashCode();
+          System.out.println(keyHash);
+          if (dis.readInt() == keyHash) {
+            dos.writeInt(KEY_CORRECT);
+          } else {
+            dos.writeInt(KEY_INCORRECT);
+            break;
+          }
+
+
+          // Despues de la conexion, Servidor y Cliente deben ponerse de acuerdo
+          // para ver quien escribe primero y entonces el otro debe leer
+          BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+
+
+
+          // Como el Cliente escribe, yo debo leer
+          int bytesLeidos = dis.read(peticion);
+          System.out.println("bytes leidos: " + bytesLeidos);
+          byte peticion2[]  = new byte[bytesLeidos];
+          for(int i=0; i < bytesLeidos; i++ )
+          {
+            peticion2[i] = peticion[i];
+          }
+
+          Cipher cifrar = Cipher.getInstance("DES");
+          cifrar.init(Cipher.DECRYPT_MODE, llave);
+          System.out.println("");
+          bytesToBits( peticion2 );
+          byte[] newPlainText = cifrar.doFinal(peticion2);
+          System.out.print( "El mensaje DESENCRIPTADO es: " );
+          // NO SE DEBE PASAR A String
+          // System.out.println( new String(newPlainText, "UTF8") );
+          for(int i=0; i < newPlainText.length; i++)
+            System.out.print( (char)newPlainText[i] );
+          
+          System.out.println("");
+          System.out.println( "\n===========================" );
+          System.out.print("> ");
+          String respuesta = br.readLine();
+          System.out.println( "Mi respuesta es: " + respuesta );
+          System.out.println( "Ahora encriptamos la respuesta..." );
+          byte[] arrayRespuesta = respuesta.getBytes();
+          cifrar = Cipher.getInstance("DES");
+          cifrar.init(Cipher.ENCRYPT_MODE, llave);
+          byte[] cipherText = cifrar.doFinal( arrayRespuesta );
+          System.out.println( "El argumento ENCRIPTADO es:" );
+          // NO SE DEBE PASAR A String
+          // System.out.println( new String( cipherText ) );
+          for(int i=0; i < cipherText.length; i++)
+            System.out.print( (char)cipherText[i] );
+          System.out.println( "" );
+          // Como yo escribo la peticion a la red,
+          // el Servidor debe leer de la red
+          bytesToBits( cipherText );
+          dos.write( cipherText, 0, cipherText.length );
+          System.out.println("\n");
+        }
         dos.close();
         dis.close();
         socket.close();
@@ -102,7 +128,19 @@ public class ServidorCifrado01
     }
   }
 
-  public static void bytesToBits( byte[] texto )
+  static byte[] cifrar(byte[] texto, Key llave) throws Exception {
+    Cipher cifrar = Cipher.getInstance("DES");
+    cifrar.init(Cipher.ENCRYPT_MODE, llave);
+    return cifrar.doFinal(texto);
+  }
+
+  static byte[] descifrar(byte[] textoCifrado, Key llave) throws Exception {
+    Cipher descifrar = Cipher.getInstance("DES");
+    descifrar.init(Cipher.DECRYPT_MODE, llave);
+    return descifrar.doFinal(textoCifrado);
+  }
+
+  static void bytesToBits( byte[] texto )
   {
     StringBuilder stringToBits = new StringBuilder();
     for( int i=0; i < texto.length; i++ )
@@ -119,5 +157,10 @@ public class ServidorCifrado01
       stringToBits.append( binary );
     }
     System.out.println( "El mensaje completo en bits es:" + stringToBits );
+  }
+
+  static void printError(String msg) {
+    System.out.println(msg);
+    System.exit(1);
   }
 }
